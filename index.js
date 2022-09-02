6a8ef2277ce438755cf4ad4d7412c1cc67bb6657
Kenia Balestra <kenia.balestra@gmail.com>
2022-09-02 16:37:03 +0000

    toDoList eslinted

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// const taskTitles = [];
+// const taskComplete = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title, description) {
+const newTask = function(title, description) {
   const task = {
     title: title,
     description: description,
@@ -22,8 +22,8 @@ function newTask(title, description) {
 
 // DRIVER CODE BELOW
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
-const task2 = newTask("Do Laundry", "😨");
-const tasks = [task1, task2];
+// const task2 = newTask("Do Laundry", "😨");
+// //const tasks = [task1, task2];
 
 
 task1.logTaskState(); // Clean Cat Litter has not been completed
